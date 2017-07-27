9616f7fc8cb0c1886846ef82570889fa42cf3d3d
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-07-27 14:46:17 +0100

    Make instructor section dynamic

--- wp-content/themes/bootstrap2wordpress/page-home.php
+++ wp-content/themes/bootstrap2wordpress/page-home.php
@@ -32,6 +32,17 @@ $features_section_body = get_field('features_section_body');
 $project_features_title = get_field('project_feature_title');
 $project_features_body = get_field('project_feature_body');
 
+$instructor_section_title = get_field('instructor_section_title');
+$instructor_section_name = get_field('instructor_section_name');
+$bio_exerpt = get_field('bio_exerpt');
+$full_bio = get_field('full_bio');
+$twitter_username = get_field('twitter_username');
+$facebook_username = get_field('facebook_username');
+$google_plus_username = get_field('google_plus_username');
+$num_students = get_field('num_students');
+$num_reviews = get_field('num_reviews');
+$num_courses = get_field('num_courses');
+
 get_header(); ?>
 
         <!-- Hero -->
@@ -207,55 +218,36 @@ get_header(); ?>
                         <div class="row">
                             <div class="col-lg-8">
                                 <h2>
-                                    Your Instructor 
+                                    <?php echo $instructor_section_title; ?>
                                     <small>
-                                        Brad Hussey
+                                        <?php echo $instructor_section_name; ?>
                                     </small>
                                 </h2>
                             </div>
                             <div class="col-lg-4">
-                                <a href="https://twitter.com/bradhussey" target="_blank" class="badge social twitter">
-                                    <i class="fa fa-twitter"></i>
-                                </a>
-                                <a href="https://facebook.com/bradhussey" target="_blank" class="badge social facebook">
-                                    <i class="fa fa-facebook"></i>
-                                </a>
-                                <a href="https://plus.google.com/+Bradhussey" target="_blank" class="badge social g-plus">
-                                    <i class="fa fa-google-plus"></i>
-                                </a>
+                                <?php if(!empty($twitter_username)): ?>
+                                    <a href="https://twitter.com/<?php echo $twitter_username; ?>" target="_blank" class="badge social twitter">
+                                        <i class="fa fa-twitter"></i>
+                                    </a>
+                                <?php endif ?>
+                                <?php if(!empty($facebook_username)): ?>
+                                    <a href="https://facebook.com/<?php echo $facebook_username; ?>" target="_blank" class="badge social facebook">
+                                        <i class="fa fa-facebook"></i>
+                                    </a>
+                                <?php endif ?>
+                                <?php if(!empty($google_plus_username)): ?>
+                                    <a href="https://plus.google.com/<?php echo $google_plus_username; ?>" target="_blank" class="badge social g-plus">
+                                        <i class="fa fa-google-plus"></i>
+                                    </a>
+                                <?php endif ?>
                             </div>
                         </div>
                         <p class="lead">
-                            A highly skilled professional, Brad Hussey is a 
-                            passionate and experienced web designer, developer, 
-                            blogger and digital entrepreneur.
+                            <?php echo $bio_exerpt; ?>
                         </p>
 
                         <p>
-                            Hailing from North Of The Wall (Yellowknife, Canada)
-                            , Brad made the trek to the Wet Coast (Vancouver, 
-                            Canada) to educate and equip himself with the 
-                            necessary skills to become a spearhead in his trade 
-                            of solving problems on the web, crafting design 
-                            solutions, and speaking in code.
-                        </p>
-                        
-                        <p>
-                            Brad's determination and love for what he does has 
-                            landed him in some pretty interesting places with 
-                            some neat people. He's had the privilege of working 
-                            with, and providing solutions for, numerous 
-                            businesses, big &amp; small, across the Americas.
-                        </p>
-                        
-                        <p>
-                            Brad builds custom websites, and provides design 
-                            solutions for a wide-array of clientele at his 
-                            company, Brightside Studios. He regularly blogs 
-                            about passive income, living your life to the 
-                            fullest, and provides premium quality web design 
-                            tutorials and courses for tens of thousands of 
-                            amazing people desiring to master their craft.
+                            <?php echo $full_bio; ?>
                         </p>
                         
                         <hr>
@@ -271,21 +263,21 @@ get_header(); ?>
                             <div class="col-xs-4">
                                 <div class="num">
                                     <div class="num-content">
-                                        41,000+ <span>students</span>
+                                        <?php echo $num_students; ?> <span>students</span>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-xs-4">
                                 <div class="num">
                                     <div class="num-content">
-                                        568 <span>reviews</span>
+                                        <?php echo $num_reviews; ?> <span>reviews</span>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-xs-4">
                                 <div class="num">
                                     <div class="num-content">
-                                        8 <span>courses</span>
+                                        <?php echo $num_courses; ?> <span>courses</span>
                                     </div>
                                 </div>
                             </div>
